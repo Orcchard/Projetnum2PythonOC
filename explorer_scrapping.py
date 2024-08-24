41e5a77386b6c592dd4f807c78e188d91d010b34
dahbia zaidi <orchard@noos.fr>
2024-08-25 00:27:54 +0200

    récuperation des URL de categorie Sequential Art et remplacement des caracteres spéciaux

--- explorer_scrapping.py
+++ explorer_scrapping.py
@@ -5,7 +5,7 @@ import string
 url = 'https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 
 response = requests.get(url)
-#links[]
+
 soup = BeautifulSoup(response.content, 'lxml')
 
 tds= soup.findAll('td')
@@ -25,4 +25,18 @@ with open('exploration.txt', 'w') as file:
     file.write(str(tds)+ '\n')
     file.write(str(ths))
     file.write(str(trs)+ '\n')
-             
+links =[]
+url ='https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html'
+response = requests.get(url)
+if response.ok:
+    print(response)
+soup =  BeautifulSoup(response.text, 'lxml')
+tds = soup.findAll('h3') 
+for h3 in tds :
+        a = h3.find('a')
+        link = a['href']
+        links.append('https://books.toscrape.com/' + link)
+for full_url in links:  
+    full_url_replaced = full_url.replace("/../../../", "/catalogue/")     
+    with open('results_url.txt', 'w') as file:
+        file.write(full_url_replaced +'\n')             
