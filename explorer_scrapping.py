5a123ecb148076657c23f97a28ac71960cdeb3b5
dahbia zaidi <orchard@noos.fr>
2024-08-24 02:30:07 +0200

    recherche td tr th

--- explorer_scrapping.py
+++ explorer_scrapping.py
@@ -12,7 +12,7 @@ tds= soup.findAll('td')
 print(len(tds)) 
 [print(str(td) + '\n\n') for td in tds]
 
-"""
+
 ths= soup.findAll('th')
 print(len(ths)) 
 [print(str(th) + '\n\n') for th in ths]
@@ -21,10 +21,8 @@ trs= soup.findAll('tr')
 print(len(trs)) 
 [print(str(tr) + '\n\n') for tr in trs]
 
-   """
 with open('exploration.txt', 'w') as file:
     file.write(str(tds)+ '\n')
-               # file.write(str(ths))
-                
-                #file.write(str(trs)+ '\n')
+    file.write(str(ths))
+    file.write(str(trs)+ '\n')
              
