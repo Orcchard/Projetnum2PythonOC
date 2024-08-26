c91ff78414c1e7243e318a17f38b01e7ec1b579f
dahbia zaidi <orchard@noos.fr>
2024-08-26 11:20:38 +0200

    Obtention des 4 pages de la catégory mais toujours erreur concatenation list et str

--- explorer_scrapping.py
+++ explorer_scrapping.py
@@ -1,5 +1,5 @@
 import requests
-#import csv
+import csv
 from bs4 import BeautifulSoup
 from urllib.request import urlopen as uReq
 import time
@@ -9,21 +9,22 @@ links =[]
 for i in range(5):
     c = "page-" + str(i) + ".html" 
     url ="https://books.toscrape.com/catalogue/category/books/sequential-art_5/"+ c
-response = requests.get(url)
-if response.ok:
-    print(response)
-print('page :'+ str(i))
-    
-soup =  BeautifulSoup(response.text, 'lxml')
-#on search les elements h3 à inside la page
-tds = soup.findAll('h3') 
+    response = requests.get(url)
+    if response.ok:
+        print('page :'+ str(i))
+        print(response)
+    else:
+        print(f"wrong pas de resultat.Status:{response.status_code}")  
+        soup =  BeautifulSoup(response.text, 'lxml')
+#on search les elements h3 à inside la page 
+        tds = soup.findAll('h3') 
 #pour each element td on va search le a
-for h3 in tds :
-    a = h3.find('a')
-    link = a['href']
+        for h3 in tds :
+            a = h3.find('a')
+            link = a['href']
 #on reconstitute le lien à link created en amont
-    links.append('https://books.toscrape.com/' + link)
-    time.sleep(2)
-print(links)
-with open('results_url.txt', 'w') as file:
-    file.write(links +'\n')    
+            links.append('https://books.toscrape.com/' + link)
+            time.sleep(2)
+            print(links)
+        #with open('results_url.txt', 'w') as file:
+            #file.write(links +'\n')    
