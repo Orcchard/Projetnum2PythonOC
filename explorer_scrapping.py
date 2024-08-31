f7d6a20c12fff697fddeeca3e23486f464a35e7e
dahbia zaidi <orchard@noos.fr>
2024-08-31 16:09:10 +0200

    Plusieurs test pour trouver pourquoi writ.file ne donne pas la même chose que print

--- explorer_scrapping.py
+++ explorer_scrapping.py
@@ -1,27 +1,35 @@
 import requests
 import csv
 from bs4 import BeautifulSoup
-from urllib.request import urlopen as uReq
 import time
 #import string
 links =[]
 #la category sequential art a 4 pages
 for i in range(5):
-    c = "page-" + str(i) + ".html" 
+    c = (f"page-{str(i)}.html" )
+    print(c)
     url ="https://books.toscrape.com/catalogue/category/books/sequential-art_5/"+ c
     response = requests.get(url)
     if response.ok:
-        print(response) 
-        print(url)
-        soup =  BeautifulSoup(response.text, 'lxml')
+        print(f"response est de type: {type(response)}")
+    print(response) 
+    print(url)
+    soup =  BeautifulSoup(response.content, 'lxml')
+    print(f"soup est de type: {type(soup)}")
 #on search les elements h3 à inside la page 
 tds = soup.findAll('h3') 
+print(f"h3 est de type: {type('h3')}")
 #pour each element td on va search le a
 for h3 in tds :
-            a = h3.find('a')
-            link = a['href']
+    a = h3.find('a')
+    link = a['href']
 #on reconstitute le lien à link created en amont
-            links.append('https://books.toscrape.com/' + link.replace("../../.." , "catalogue"))
-            time.sleep(2)
-with open('results_url.txt', 'w') as file:
-            file.write(links +'\n')
+    links.append('https://books.toscrape.com/' + link.replace("../../.." , "catalogue"))
+time.sleep(2)
+
+for url in links:
+    print(url)
+    print(f"url est de type: {type(url)}")          
+    with open('results_url.txt', 'w') as file:
+        file.write(url+'\n')
+        
