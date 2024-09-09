57db8ca6ce6cbc63d06329beca5d828860c10f86
dahbia zaidi <orchard@noos.fr>
2024-09-09 23:39:41 +0200

    Je peux enfin voir mes catégories défiler

--- explorer_scrapping.py
+++ explorer_scrapping.py
@@ -5,7 +5,7 @@ import re
 import os 
 from pathlib import Path
 import time
-# URL de la première page de la catégorie "Sequential Art"
+# URL de la première result de la catégorie "Sequential Art"
 start_time = time.perf_counter()
 depart_url = "https://books.toscrape.com/"
 catalog_url = "http://books.toscrape.com/catalogue/"
@@ -13,21 +13,66 @@ catalog_url = "http://books.toscrape.com/catalogue/"
 links_categ = []
 response = requests.get(depart_url)
 if response.ok:   
-# Parser le contenu HTML de la page
+# Parser le contenu HTML de la result
     soup =  BeautifulSoup(response.content, 'lxml')
 #print(f"soup est de type: {type(soup)}")
-#on search les elements  à inside la page 
+#on search les elements  à inside la result 
     list_categ = soup.find('div', {'class': 'side_categories'}).find_all('li')
     for little_a in list_categ:
         a = little_a.find('a')
         linka = a['href']
 #on reconstitute le lien à link created en amont
         links_categ.append(depart_url + linka)
-    time.sleep(2)          
-    with open('results_url.txt', 'w') as file:
-        for uerl in links_categ:
-            file.write(uerl+'\n')
+        
+#Note: the [1:] is to skip the first category that regroups ALL the books of the website
+##Remove the first row in the list
+for by_category in links_categ[1:]:
+    result = requests.get(by_category)
+    category_sequential_art = by_category
+    #time.sleep(2) 
+    links = []
+   
+#Getting the type of category (will be used for the creation of the directory as well as the output CSV file)
+    if result.ok:
+        soup = BeautifulSoup(result.content, 'lxml')
+        which_category = soup.find('li', class_='active').text
+        print("The category is : " + which_category)  
+    #Extracting the links depending on if there are several pages or only one
+    #if result.ok:
+        #soup = BeautifulSoup(result.text, 'html.parser')
+        next_page = soup.find_all('ul', class_='pager')
+        print(next_page)
 """
+    #More than one result:
+        if len(next_page) >=1 :
+            for num_of_pages in next_page:
+                num_of_pages2 = num_of_pages.find('li', class_="current").text
+                test = num_of_pages2.strip()[10:]
+                test2 = int(test)+1
+                print(f"This category has {test2-1} pages in total.")
+                
+                for i in range(1, test2):
+                    url2 = category_mystery.replace('index.html', '') + f"result-{str(i)}.html"
+                    page2 = requests.get(url2)
+                    print(page2)
+                    print(url2)
+                    if page2.ok:
+                        print("result: " + str(i))
+                        soup = BeautifulSoup(page2.text, 'html.parser')
+                        book_title = soup.find_all('article', class_='product_pod')
+                        for titlelink in book_title:
+                            a = titlelink.find('a')
+                            book_link = a['href']
+                            links.append(catalog_url + book_link[9:])
+
+    
+    
+    
+    
+    
+    
+    
+   
         rev_rating = soup.find("p", class_= "star-rating").get("class")[-1]
         table = soup.find('table', class_='table table-striped')
         upc= []
