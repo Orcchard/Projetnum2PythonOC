e466f1b32dea35d42db25a2b3b1c48073eb87e53
dahbia zaidi <orchard@noos.fr>
2024-09-16 16:24:47 +0200

    J'ai enfin distingué les categories avec plusieurs pages

--- explorer_scrapping.py
+++ explorer_scrapping.py
@@ -5,159 +5,95 @@ import re
 import os 
 from pathlib import Path
 import time
-# URL du site  de vente en ligne books.toscrap 
-start_time = time.perf_counter()
-depart_url = "https://books.toscrape.com/"
-catalog_url = "http://books.toscrape.com/catalogue/"
-# Variable  pour stocker les URLs récupérées sous forme de liste
-links_categ = []
-response = requests.get(depart_url)
-if response.ok:   
-# Parser le contenu HTML de la result
-    soup =  BeautifulSoup(response.content, 'lxml')
-#print(f"soup est de type: {type(soup)}") test comprehension
-# chargement ds variable ldu resultat de find
-    list_categ = soup.find('div', {'class': 'side_categories'}).find_all('li')
-    for little_a in list_categ:
-        a = little_a.find('a')
-        linka = a['href']
-#on reconstitute la liste des urls de toutes les categories
-        links_categ.append(depart_url + linka)    
-#Note: le [1:] permet de commencer les categories du site par la première visible dans la liste (Travel)
-for by_category in links_categ[1:]:
-    result = requests.get(by_category)
-    all_categories = by_category
-    #time.sleep(2) 
-    links = []
-#Permet de retrouver plus finement chaque catégorie
-    if result.ok:
-        soup = BeautifulSoup(result.content, 'lxml')
-        which_category = soup.find('li', class_="active").text
-        #print(f"The category is : {which_category}")
-    #On recherche si le nombre de pages ets superieur à une page
-    #if result.ok:
-        #soup = BeautifulSoup(result.text, 'html.parser')
-        # Recherche de la page suivante
-        next_page = soup.find_all('ul', class_="pager")
-        #print(next_page)
-    #more than one result  
-        if len(next_page)>=1:
-            for page_num in next_page:
-                page_num_suiv = page_num.find('li' , class_="current").text
-                #print(len(page_num_suiv))
-                #print(page_num_suiv)
-                #Permet d'extraire le nombre de pages de la catégorie à partir du 10 eme caratère de la liste
-                choice = page_num_suiv.strip()[10:]
-                #print(choice)
-                choice_suiv = int(choice)+1
-                #print(f"choice_suiv : {choice_suiv} alors que choice : {choice}")
-                #print(f"This category has {choice_suiv-1} pages in total.")
-                #Boucle sur le nombre de pages entre 1 et dernière page + 1 et extraction de toutes les url des pages de chaque categorie
-                for i in range(1, choice_suiv):
-                    url_page = all_categories.replace('index.html', '') + f"page-{str(i)}.html"
-                    page_after = requests.get(url_page)
-                    #print(f"page_after : {page_after}")
-                    #print(f"url_page  : {url_page}")
-                    
-                    if page_after.ok:
-                        print("result: " + str(i))
-                        soup = BeautifulSoup(page_after.content, 'lxml')
-                        book_title = soup.find_all('article', class_="product_pod")
-                        for titlelink in book_title:
-                            a = titlelink.find('a')
-                            book_link = a['href']
-                            title_net = book_link.replace("../../../", "")
-                            links.append(catalog_url + title_net)
-                            for def_links in links:
-                                print(f"nombre de pages = {choice_suiv} pour url : {def_links}")
-    # s'il n'y a qu'une page?
-    
-        else:
-            url_page = all_categories
-            page_after = requests.get(url_page)  
-            #print(page_after)
-            if page_after.ok:
-               soup = BeautifulSoup(page_after.text, 'html.parser')
-               book_title = soup.findAll('article')
-               for titlelink in book_title:
-                    a = titlelink.find('a')
-                    book_link = a['href']          
-                    title_net2 = book_link.replace("../../../", "")
-                    links.append(catalog_url + title_net2)
-                    for def_links2 in links:
-                        print(f"pour url : {def_links2}")
-"""
-        rev_rating = soup.find("p", class_= "star-rating").get("class")[-1]
-        table = soup.find('table', class_="table table-striped")
-        upc= []
-        upc_row = table.find('th', string='UPC')
-# Extraire le texte de la cellule linked
-        upc = upc_row.find_next_sibling('td').text
-    for row in file:
-        url = row.strip()
-        print(url + ' , ' + rev_rating + ' , '+ upc_row)
-# response = requests.url
-        #if response.ok:         
-        #rev_rating = soup.find("p", class_= "star-rating").get("class")[-1] 
-        if rev_rating == "One":  # on adapte le chiffre obtenu avec une note sur cinq en string.
-            rev_rating = "1 / 5"
-        elif rev_rating == "Two":
-            rev_rating = "2 / 5"
-        elif rev_rating == "Three":
-            rev_rating = "3 / 5"
-        elif rev_rating == "Four":
-            rev_rating = "4 / 5"    
-        elif rev_rating == "Five":
-                rev_rating = "5 / 5"
-        else:
-            rev_rating = "0 / 5"
-#extraire l'UPC du livre de la catégorie
-table = soup.find('table', class_='table table-striped')
-if table: 
-    upc= []
-    upc_row = table.find('th', string='UPC')
-# Extraire le texte de la cellule linked
-    upc = upc_row.find_next_sibling('td').text
-    print(upc)
-#récuperer l'url ou figure le livre selectionné
-#url_category= url
-#print(url_category)
-#recuperer le prix hors taxe puis TTC
-#price_ht = soup.find('th', string = "Price (excl. tax)").find('td').string
-#price_ttc = soup.find('th',string="Price (incl. tax)").find_next_sibling("td").string
-#print(price_ht)
-#print(price_ttc)
-#extraire Number available
-product_avaib = soup.find('th',string="Availability").find_next_sibling("td").string
-print(product_avaib.replace("(" , ":"))
-#extraire la catégorie
-breadcrumb =soup.find('ul', class_="breadcrumb")
 
-#print(breadcrumb)
-category = breadcrumb.find_all('a')
-categorie=category[-1].text.strip()
-print(len('categorie'))
-print(categorie)
-#créer l'url de l' image du livre selectionné
-picture = soup.find("img")
-picture_url = picture["src"]
-print(picture_url)
-image_url = "https://books.toscrape.com/" + picture_url.replace("../../", "")
-print(image_url)
-#création d'un dictionnaire pour stocker les données du livre sélectionné
-data_produit = {"product_page_url" : row,
-                "universal_product_code" : upc,
-                "title" : title,
-                "price_including_tax" : price_ttc,
-                "price_excluding_price" : price_ht,
-                "number_available" : product_avaib,
-                "category" : categorie,
-                "review_rating" : rev_rating,
-                "image_url" : image_url,
-                }
+#||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+#extraire les information d'un livre de la rubrique Sequential Art
+links =[]
+url ="https://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'lxml')
+if response.ok:
+    product_page_url = url
+    product_information = soup.find_all('td')
+    p = soup.find_all('p')
+    title = soup.find('h1').text
+    p_all = soup.find_all('p')
+    upc = product_information[0].text
+    title = soup.find('h1').text
+    price_including_tax = product_information[2].text
+    price_excluding_tax = product_information[3].text
+    prod_avail = product_information[5].text
+    number_available = prod_avail.replace( ' (', ' ').replace(')', '')
+    product_description = soup.find('div', {'id': 'product_description'}).find_next('p').text
+    category = soup.find_all('a')[3].text
+    review_rating = f"{p[-1]['class'][-1]} stars"
+    picture = soup.find("img")
+    picture_url = picture["src"]
+    print(picture_url)
+    image_url = "https://books.toscrape.com/" + picture_url.replace("../../", "")
+
+    print(f"url : {url}")
+    print(f"UPC : {upc}")
+    print(f"Titre : {title}")
+    print(f"price_including_tax : {price_including_tax}")
+    print(f"price_excluding_tax : {price_excluding_tax}")
+    #print(f"Prod avail : {prod_avail}")
+    print(f"number available : {number_available}")
+    print(f"product description : {product_description}")
+    print(f"category: {category}")
+    print(f"review rating : {review_rating}")
+    print(f"image url : {image_url}")
+    
+    #ecrire les données dans un dictionnaire
+    data_produit = {
+        "product_page_url" : url ,
+        "universal_product_code" : upc,
+        "title" : title,
+        "price_including_tax" : price_including_tax, 
+        "price_excluding_price" : price_excluding_tax, 
+        "number_available" : number_available, 
+        "product description" : product_description, 
+        "category" : category, 
+        "review_rating" : review_rating, 
+        "image_url" : image_url, 
+    }
 #ecrire les data dans un fichier csv
-with open('product_information.csv', 'w') as file:
-        writer = csv.DictWriter(file , fieldnames=data_produit.keys())
+with open('product_information.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file ,fieldnames=data_produit.keys())
         writer.writeheader()
-        writer.writerow(data_produit)  
-"""
+        writer.writerow(data_produit)
+#|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+#recuperer toutes les urls de la catégorie sequential art sur 4 pages et ses elements
+#je créé une variable
+sequential_links =[]
+#la category sequential art a 4 pages
+
+for i in range(5):
+    c = (f"page-{str(i)}.html" )
+    print(c)
+
+    #je recupere les infos des pages
+    url ="https://books.toscrape.com/catalogue/category/books/sequential-art_5/"+ c
+    response = requests.get(url)
+    if response.ok:
+        print(f"response est de type: {type(response)}")
+        print(response) 
+        print(url)
+        soup =  BeautifulSoup(response.content, 'lxml')
+        print(f"soup est de type: {type(soup)}")
+#on search les elements h3 à inside la page 
+        tds = soup.find_all('h3') 
+        print(f"h3 est de type: {type('h3')}")
+#pour each element td on va search le a
+        for h3 in tds :
+            a = h3.find('a')
+            link = a['href']
+#on reconstitute le lien à link created en amont
+            sequential_links.append('https://books.toscrape.com/' + link.replace("../../.." , "catalogue"))
+            
+            time.sleep(2)
+
+            with open('results_url.txt', 'w') as file:
+                for uerl in sequential_links:
+                    file.write(uerl+'\n')
+                    print(f"Titre de livre:{uerl}")
